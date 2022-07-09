3e4e73378d893894a674f844f52c772584c80c38
AnthonyMcGreal <anthonymcgreal@hotmail.co.uk>
2022-07-09 16:55:32 +0100

    add ingredient id to select recipe by id

--- mvcs/models/recipeModels.js
+++ mvcs/models/recipeModels.js
@@ -1,129 +1,129 @@
-const db = require('../../db/connection');
-const format = require('pg-format');
+const db = require('../../db/connection')
+const format = require('pg-format')
 const {
-  deleteIngredientsById,
-} = require('../controllers/ingredientsController');
-const { query } = require('../../db/connection');
+	deleteIngredientsById
+} = require('../controllers/ingredientsController')
+const { query } = require('../../db/connection')
 
 exports.selectAllRecipes = () => {
-  let queryStr = `SELECT DISTINCT recipe_name FROM recipes;`;
+	let queryStr = `SELECT DISTINCT recipe_name FROM recipes;`
 
-  return db.query(queryStr).then(({ rows }) => {
-    return rows;
-  });
-};
-exports.selectRecipeById = (name) => {
-  let queryStr = format(
-    `SELECT recipes.recipe_id, recipes.recipe_name, recipes.link, ingredients.name, recipes.ingredient_quantity, ingredients.unit_of_measurement, recipes.portions, ingredients.storage_type FROM recipes
+	return db.query(queryStr).then(({ rows }) => {
+		return rows
+	})
+}
+exports.selectRecipeById = name => {
+	let queryStr = format(
+		`SELECT recipes.recipe_id, recipes.recipe_name, recipes.link, recipes.ingredients, ingredients.name, recipes.ingredient_quantity, ingredients.unit_of_measurement, recipes.portions, ingredients.storage_type FROM recipes
   JOIN ingredients
   ON recipes.ingredients = ingredients.ingredient_id
   WHERE recipe_name = %L`,
-    [name]
-  );
+		[name]
+	)
 
-  return db.query(queryStr).then(({ rows }) => {
-    return rows;
-  });
-};
+	return db.query(queryStr).then(({ rows }) => {
+		return rows
+	})
+}
 
-exports.insertRecipe = async (body) => {
-  let recipeName = body[0].recipe_name;
+exports.insertRecipe = async body => {
+	let recipeName = body[0].recipe_name
 
-  const insertIngredients = async () => {
-    return Promise.all(
-      body.map((ingredient) => {
-        let {
-          recipe_name,
-          username,
-          link,
-          ingredients,
-          ingredient_quantity,
-          portions,
-        } = ingredient;
+	const insertIngredients = async () => {
+		return Promise.all(
+			body.map(ingredient => {
+				let {
+					recipe_name,
+					username,
+					link,
+					ingredients,
+					ingredient_quantity,
+					portions
+				} = ingredient
 
-        let queryStr = format(
-          `INSERT INTO recipes
+				let queryStr = format(
+					`INSERT INTO recipes
       (recipe_name, username, link, ingredients, ingredient_quantity, portions)
         VALUES
         %L
         RETURNING *;`,
-          [
-            [
-              recipe_name,
-              username,
-              link,
-              ingredients,
-              ingredient_quantity,
-              portions,
-            ],
-          ]
-        );
+					[
+						[
+							recipe_name,
+							username,
+							link,
+							ingredients,
+							ingredient_quantity,
+							portions
+						]
+					]
+				)
 
-        return db.query(queryStr);
-      })
-    );
-  };
+				return db.query(queryStr)
+			})
+		)
+	}
 
-  await insertIngredients();
+	await insertIngredients()
 
-  let queryStr = format(
-    `SELECT recipe_name, username, link, ingredients, ingredient_quantity, portions FROM recipes WHERE recipe_name = %L;`,
-    [recipeName]
-  );
+	let queryStr = format(
+		`SELECT recipe_name, username, link, ingredients, ingredient_quantity, portions FROM recipes WHERE recipe_name = %L;`,
+		[recipeName]
+	)
 
-  return db.query(queryStr).then(({ rows }) => {
-    return rows;
-  });
-};
+	return db.query(queryStr).then(({ rows }) => {
+		return rows
+	})
+}
 
-exports.updateRecipeByName = async (body) => {
-  let recipeName = body[0].name;
+exports.updateRecipeByName = async body => {
+	let recipeName = body[0].name
 
-  const updateRecipe = async () => {
-    return Promise.all(
-      body.map((ingredient) => {
-        let {
-          recipe_id,
-          name,
-          username,
-          link,
-          ingredients,
-          ingredient_quantity,
-          portions,
-        } = ingredient;
+	const updateRecipe = async () => {
+		return Promise.all(
+			body.map(ingredient => {
+				let {
+					recipe_id,
+					name,
+					username,
+					link,
+					ingredients,
+					ingredient_quantity,
+					portions
+				} = ingredient
 
-        return db.query(
-          `UPDATE recipes
+				return db.query(
+					`UPDATE recipes
           SET recipe_name = $2, username = $3, link = $4, ingredients = $5, ingredient_quantity = $6, portions = $7
           WHERE recipe_id = $1
           RETURNING *;`,
-          [
-            recipe_id,
-            name,
-            username,
-            link,
-            ingredients,
-            ingredient_quantity,
-            portions,
-          ]
-        );
-      })
-    );
-  };
+					[
+						recipe_id,
+						name,
+						username,
+						link,
+						ingredients,
+						ingredient_quantity,
+						portions
+					]
+				)
+			})
+		)
+	}
 
-  await updateRecipe();
+	await updateRecipe()
 
-  let queryStr = format(`SELECT * FROM recipes WHERE recipe_name = %L`, [
-    recipeName,
-  ]);
+	let queryStr = format(`SELECT * FROM recipes WHERE recipe_name = %L`, [
+		recipeName
+	])
 
-  return db.query(queryStr).then(({ rows }) => {
-    return rows;
-  });
-};
+	return db.query(queryStr).then(({ rows }) => {
+		return rows
+	})
+}
 
-exports.removeRecipeByName = (name) => {
-  return db.query('DELETE FROM recipes WHERE recipe_name = $1 RETURNING *;', [
-    name,
-  ]);
-};
+exports.removeRecipeByName = name => {
+	return db.query('DELETE FROM recipes WHERE recipe_name = $1 RETURNING *;', [
+		name
+	])
+}
