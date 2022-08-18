a9430a298a89f0372beace95c868901fe80889a0
AnthonyMcGreal <anthonymcgreal@hotmail.co.uk>
2022-08-18 22:00:10 +0100

    update data

--- db/data/test_data/templates.js
+++ db/data/test_data/templates.js
@@ -3,9 +3,24 @@ module.exports = [
 		name: 'Week 1 test',
 		username: 'Anthony',
 		recipes: [
-			{ Wednesday: ['Spag_Bol', 'Chilli'] },
-			{ Thursday: ['Porridge', 'Sandwiches'] },
-			{ Friday: ['Porridge', 'Curry'] }
+			{
+				Wednesday: [
+					{ portions: 2, recipe_name: 'Spag_Bol' },
+					{ portions: 2, recipe_name: 'Chilli' }
+				]
+			},
+			{
+				Thursday: [
+					{ portions: 2, recipe_name: 'Porridge' },
+					{ portions: 2, recipe_name: 'Sandwiches' }
+				]
+			},
+			{
+				Friday: [
+					{ portions: 2, recipe_name: 'Porridge' },
+					{ portions: 2, recipe_name: 'Curry' }
+				]
+			}
 		]
 	}
 ]
