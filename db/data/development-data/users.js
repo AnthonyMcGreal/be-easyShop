3ac00ce95da9052aee696fe68965c84ad9943f30
AnthonyMcGreal <anthonymcgreal@hotmail.co.uk>
2022-09-22 22:26:28 +0100

    update dev data

--- db/data/development-data/users.js
+++ db/data/development-data/users.js
@@ -1,16 +1,16 @@
 module.exports = [
 	{
-		uuid: '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d',
+		user_id: '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d',
 		email: 'anthonymcgreal@hotmail.co.uk',
 		password: 'TestPassword1'
 	},
 	{
-		uuid: '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6e',
+		user_id: '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6e',
 		email: 'sol@outlook.co.uk',
 		password: 'TestPassword2'
 	},
 	{
-		uuid: '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6f',
+		user_id: '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6f',
 		email: 'david@outlook.co.uk',
 		password: 'TestPassword3'
 	}
