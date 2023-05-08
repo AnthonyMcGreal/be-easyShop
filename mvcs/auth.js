73e24c81b8ee84eb3ddfe479a03aa7e44074abff
AnthonyMcGreal <anthonymcgreal@hotmail.co.uk>
2023-05-08 19:40:40 +0100

    update auth

--- mvcs/auth.js
+++ mvcs/auth.js
@@ -1,10 +1,9 @@
 const jwt = require('jsonwebtoken')
-const jwtSecret = process.env.JWTSECRET
 
 exports.userAuth = (req, res, next) => {
 	const token = req.headers.authorization.split(' ')[1]
 	if (token) {
-		jwt.verify(token, jwtSecret, (err, decodedToken) => {
+		jwt.verify(token, process.env.JWTSECRET, (err, decodedToken) => {
 			if (err) {
 				return res.status(401).json({ message: 'Not authorized' })
 			} else {
